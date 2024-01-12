d36986f8c27e0026369141dd3175f65b783e7f03
TwentyFifthNight <128817436+TwentyFifthNight@users.noreply.github.com>
2024-01-12 16:33:50 +0100

    Update title reviews.blade.php

--- services/app/laravel/resources/views/reviews.blade.php
+++ services/app/laravel/resources/views/reviews.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Game Revievs</title>
-    
+    <title>Game Reviews</title>
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/darkmode.css">
     <link rel="stylesheet" href="/css/bootstrap.min.css">
