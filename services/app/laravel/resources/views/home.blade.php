36d9d5d84ac8c6c682af1f56fceeee1f976f7cb1
TwentyFifthNight <128817436+TwentyFifthNight@users.noreply.github.com>
2024-01-12 16:33:24 +0100

    Update title home.blade.php

--- services/app/laravel/resources/views/home.blade.php
+++ services/app/laravel/resources/views/home.blade.php
@@ -2,9 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}>
 <head>
     <meta charset = "UTF-8">
-    <title>Game Revievs</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Game Reviews</title>
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/darkmode.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
